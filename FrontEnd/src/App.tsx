73cf560ed6cd0657a056886fae65801dd71a0313
Rasel Hossain Adib <97701792+AdibRasel@users.noreply.github.com>
2025-06-02 01:33:46 +0600

    File Folder Setup
    File Folder Setup

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import "./Style.css"
+function App() {
+
+  return (
+    <>
+
+    <h1>WordPress with Frontend Development & React Course</h1>
+  
+  
+  </>
+  )
+}
+
+export default App
