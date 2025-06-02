80596f7b661bae4c8880daa9d896fbb7143285c9
Rasel Hossain Adib <97701792+AdibRasel@users.noreply.github.com>
2025-06-02 09:54:26 +0600

    router setup and g-page host

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
-
-import "./Style.css"
+import "./Style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Home from './Page/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Page/Home';
+// ভবিষ্যতে অন্য পেজ এড করো যেমন About, Contact, ইত্যাদি
+
 function App() {
-
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+        <Route path="/WordPress_with_Frontend_Development_Course" element={<Home />} />
 
-    <Home />
-
- 
-  
-  </>
-  )
+        {/* 
+          ভবিষ্যতে নতুন Route যোগ করো 
+          <Route path="/about" element={<About />} /> 
+        */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
